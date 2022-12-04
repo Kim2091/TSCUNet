Average color fix

main_test_scunet_real_application.py
@@ -47,6 +47,8 @@ def main():
     parser.add_argument('--suffix', type=str, default=None, help='output filename suffix')
     parser.add_argument('--video', type=str, default=None, help='video output codec. if not None, output video instead of images')
     parser.add_argument('--video_res', type=str, default='1440:1080', help='video resolution to scale output to')
+    parser.add_argument('--video_presize', action='store_true', help='resize video to video_res/scale before processing')
+    parser.add_argument('--avg_color_fix_scale', type=float, default=0.0, help='fix average color of output to match input')
 
     args = parser.parse_args()
 
@@ -126,7 +128,7 @@ def main():
         suffix = f"{model_name}" if f"{scale}x_" in model_name else f"{scale}x_{model_name}"
 
     if video_input:
-        input_container = av.open(L_path)#options={'filter:v': 'yadif', 'r': '24000/1001'})
+        input_container = av.open(L_path, options={'filter:v': 'yadif', 'r': '24000/1001'})
         input_stream = input_container.streams.video[0]
         input_stream.thread_type = 'AUTO'
         # approximate number of frames with a couple seconds of headroom
@@ -182,11 +184,11 @@ def main():
                 img_count = idx
                 break
 
-            if args.video:
+            if args.video and args.video_presize:
                 img_L = cv2.resize(img_L, (int(args.video_res.split(':')[0])//scale, int(args.video_res.split(':')[1])//scale), interpolation=cv2.INTER_CUBIC)
-
-            img_L = util.uint2tensor4(img_L)
-            img_L = img_L.to(device)
+            
+            img_L_t = util.uint2tensor4(img_L)
+            img_L_t = img_L_t.to(device)
 
             # ------------------------------------
             # (2) img_E
@@ -194,13 +196,25 @@ def main():
             
             rng_state = torch.get_rng_state()
             torch.manual_seed(13)
-            img_E, _ = util.tiled_forward(model, img_L, overlap=256, scale=scale)
+            img_E, _ = util.tiled_forward(model, img_L_t, overlap=256, scale=scale)
             img_E = util.tensor2uint(img_E, args.depth)
             torch.set_rng_state(rng_state)
 
             # ------------------------------------
             # save results
             # ------------------------------------
+            if args.video:
+                img_E = cv2.resize(img_E, (int(args.video_res.split(':')[0]), int(args.video_res.split(':')[1])), interpolation=cv2.INTER_CUBIC)
+            if args.avg_color_fix_scale > 0.01:
+                img_E = util.avg_color_fix(img_E, img_L, args.avg_color_fix_scale)
+            """
+            if args.avg_color_fix_scale > 0.01:
+                img_L_t = util.uint2tensor4(cv2.resize(img_L, ((int)(img_L.shape[1] * (args.avg_color_fix_scale / scale)), (int)(img_L.shape[0] * (args.avg_color_fix_scale / scale))), interpolation=cv2.INTER_CUBIC)).to(device)
+                img_L_t, _ = util.tiled_forward(model, img_L_t, overlap=256, scale=scale)
+                img_L = util.tensor2uint(img_L_t, args.depth)
+                img_E = util.avg_color_fix(img_E, img_L, 1.0)
+            """
+            
             if args.video:
                 for packet in stream.encode(av.VideoFrame.from_ndarray(img_E, format="rgb48le")):
                     output_container.mux(packet)

models/network_scunet.py
@@ -266,7 +266,7 @@ class RRDBUpsample(nn.Module):
 
 class SCUNet(nn.Module):
 
-    def __init__(self, in_nc=3, out_nc=3, config=[2,2,2,2,2,2,2], dim=64, drop_path_rate=0.0, input_resolution=256, scale=1, state=None):
+    def __init__(self, in_nc=3, out_nc=3, config=[2,2,2,2,2,2,2], dim=64, drop_path_rate=0.0, input_resolution=256, scale=1, residual=False, state=None):
         super(SCUNet, self).__init__()
 
         if state:
@@ -278,6 +278,7 @@ class SCUNet(nn.Module):
 
             # TODO: obtain this parameter without assuming
             input_resolution = 64 if scale > 1 else 256 
+            residual = "m_res.0.weight" in state.keys()
 
             config = []
             for i in range(1,4):
@@ -293,14 +294,15 @@ class SCUNet(nn.Module):
         self.head_dim = 32
         self.window_size = 8
         self.scale = scale
+        self.residual = residual
+
+        unet_up = Upconv if scale > 1 else nn.ConvTranspose2d
 
         # drop path rate for each layer
         dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(config))]
 
         self.m_head = [nn.Conv2d(in_nc, dim, 3, 1, 1, bias=False)]
-
-        unet_up = Upconv if scale > 1 else nn.ConvTranspose2d
-
+        
         begin = 0
         self.m_down1 = [ConvTransBlock(dim//2, dim//2, self.head_dim, self.window_size, dpr[i+begin], 'W' if not i%2 else 'SW', input_resolution) 
                       for i in range(config[0])] + \
@@ -335,6 +337,8 @@ class SCUNet(nn.Module):
                     [ConvTransBlock(dim//2, dim//2, self.head_dim, self.window_size, dpr[i+begin], 'W' if not i%2 else 'SW', input_resolution) 
                       for i in range(config[6])]
 
+        if self.residual:
+            self.m_res = [nn.Conv2d(dim, dim, 3, 1, 1, bias=False)]
         if self.scale > 1:
             self.m_upsample = [RRDBUpsample(dim, nb=config[7], scale=self.scale)]
 
@@ -350,6 +354,8 @@ class SCUNet(nn.Module):
         self.m_up2 = nn.Sequential(*self.m_up2)
         self.m_up1 = nn.Sequential(*self.m_up1)
 
+        if self.residual:
+            self.m_res = nn.Sequential(*self.m_res)
         if scale > 1:
             self.m_upsample = nn.Sequential(*self.m_upsample)
 
@@ -367,10 +373,10 @@ class SCUNet(nn.Module):
         paddingBottom = int(np.ceil(h/64)*64-h)
         paddingRight = int(np.ceil(w/64)*64-w)
         if not self.training:
-            paddingLeft += 32
-            paddingTop += 32
-            paddingBottom += 32
-            paddingRight += 32
+            paddingLeft += 64
+            paddingTop += 64
+            paddingBottom += 64
+            paddingRight += 64
         x0 = nn.ReplicationPad2d((paddingLeft, paddingRight, paddingTop, paddingBottom))(x0)
 
         x1 = self.m_head(x0)
@@ -381,6 +387,9 @@ class SCUNet(nn.Module):
         x = self.m_up3(x+x4)
         x = self.m_up2(x+x3)
         x = self.m_up1(x+x2)
+
+        if self.residual:
+            x1 = self.m_res(x1)
 
         x = x + x1
         if self.scale > 1: